c9252a7b98cfaee98ef73d622ef234ab3f1028c7
allandhino <allandhino.pattras@dattabot.io>
2017-08-29 16:42:35 +0700

    init test

--- Sparse/lib/mongo.py
+++ Sparse/lib/mongo.py
@@ -4,7 +4,7 @@ import datetime
 
 # please note, dont connect to the driver if this function inside your loop
 def mongo_conn():
-	conn_read = MongoClient('mongodb://10.10.21.4:27017,10.10.21.212:27017/?connectTimeoutMS=300000')
+	conn_read = MongoClient('mongodb://localhost:27017/?connectTimeoutMS=300000')
 	return conn_read.db_Training_Python
 
 # function mongo_local_date() convert mongo date to local date 
